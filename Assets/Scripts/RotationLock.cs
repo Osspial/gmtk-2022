8c3f5fd412e2c2da2e9a825fe9be01b9bf44dfcb
Osspial <git@osspial.net>
2022-07-16 12:05:07 -0400

    Add player and cobble art

--- Assets/Scripts/RotationLock.cs
+++ Assets/Scripts/RotationLock.cs
@@ -7,7 +7,15 @@ public class RotationLock : MonoBehaviour
     public enum LockMode
     {
         Zero,
+        LookAtCamera,
+        LookAtCameraReverse,
     }
+    public bool ignoreX = false;
+    public bool ignoreY = false;
+    public bool ignoreZ = false;
+
+    public Vector3 offset = Vector3.zero;
+
     public LockMode mode = LockMode.Zero;
     // Start is called before the first frame update
     void Start()
@@ -18,12 +26,25 @@ public class RotationLock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var startRotation = transform.rotation;
         switch (mode)
         {
             case LockMode.Zero:
                 transform.rotation = Quaternion.Euler(0, 0, 0);
                 break;
+            case LockMode.LookAtCamera:
+                transform.LookAt(Camera.main.transform.position);
+                break;
+            case LockMode.LookAtCameraReverse:
+                transform.LookAt(Camera.main.transform.position);
+                transform.localRotation *= Quaternion.Euler(0, 180, 0);
+                break;
         }
-        
+
+        transform.rotation = Quaternion.Euler(
+            offset.x + (ignoreX ? startRotation.x : transform.eulerAngles.x),
+            offset.y + (ignoreY ? startRotation.y : transform.eulerAngles.y),
+            offset.z + (ignoreZ ? startRotation.z : transform.eulerAngles.z)
+        );
     }
 }
